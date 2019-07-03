a7606cb6c6ca9b769c01e306aaa5cd0ce5026a2d
thaithao69 <thaithao69@gmail.com>
2019-07-03 21:50:53 +0700

    update

--- Framework/src/test/java/com/auto/testcases/TC_001.java
+++ Framework/src/test/java/com/auto/testcases/TC_001.java
@@ -15,7 +15,7 @@ public class TC_001 extends BaseTestCase{
 		
 		LoginPage loginPage = new LoginPage(driver);
 		
-		logger.info("Starting Application: Login 1");
+		logger.info("Starting Application: Login 1234");
 		
 		loginPage.loginToCRM(CommonData.USER1, CommonData.PASSWORD1);
 		
